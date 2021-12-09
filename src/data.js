c1d360d70036c73df01ef250209cd6697b219b15
Ross McLean <ri.mcl@icloud.com>
2021-12-09 16:24:11 +0000

    Create data.js
    create dataSource in data.js

---
+++ src/data.js
@@ -0,0 +1,9 @@
+const dataSource = {
+  baseURL: 'http://localhost:3000/',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application.json'
+  }
+}
+
+export default dataSource
