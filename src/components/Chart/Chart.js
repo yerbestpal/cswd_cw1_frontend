baf06a5bba9cca962fe2271caeca92c25ce5d29f
Ross McLean <ri.mcl@icloud.com>
2021-12-16 00:57:21 +0000

    Fix import

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Dropdown, DropdownButton } from "react-bootstrap"
 import dataSource from "../../data"
 import { Chart as ChartJS } from 'chart.js/auto'
-import { Chart }            from 'react-chartjs-2'
+import { Chart } from 'react-chartjs-2'
 import { Bar } from 'react-chartjs-2'
 import { Row, Col } from "react-bootstrap"
 
